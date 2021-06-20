9153bf2d7a0ae785a5e2cadbb3247a231a6aeec7
Anjni-droid <84638390+Anjni-droid@users.noreply.github.com>
2021-06-20 17:01:36 +0100

    Version 1.0

--- cross_validator.py
+++ cross_validator.py
@@ -4,37 +4,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score, cross_val_predict, cross_validate
 from sklearn import metrics
 
-import matplotlib.pyplot as plt
-
-''' all imports '''
-import numpy as np
-import pandas as pd
-#
-# Import rdkit stuff
-#
-import rdkit
-from rdkit import Chem
-from rdkit.Chem.Draw import IPythonConsole
-from rdkit.Chem import PandasTools
-import numpy as np
-
-#
-# Import the machine learning tools
-#
-from sklearn.model_selection import train_test_split
-from sklearn.svm import SVR
-from sklearn.model_selection import cross_val_score, cross_val_predict, cross_validate
-from sklearn import metrics
-from sklearn.metrics import explained_variance_score
-#
-#Import matplot for our figures
-#
-import matplotlib.pyplot as plt
-#
-# Load the dataframe from a .csv file, stores the smiles strings for our compound and their PIC50a ctivities
-# Load the dataframe from a .csv file, stores the smiles strings for our compound and their PIC50a ctivities
-#
-''' end all imports '''
 
 
 class CrossValidator:
@@ -53,6 +22,11 @@ class CrossValidator:
         self.train_targets = None
         self.test_targets = None
 
+        self.predictions_cv = None
+
+        self.fpr = None
+        self.tpr = None
+        self.thresholds = None
         # Start the run 
         self._start()
 
@@ -60,20 +34,14 @@ class CrossValidator:
         print ('----------------------'+ 'loop '+str(self.itter)+ '----------------------')
 
         # Splits input data and assigns it to class variables
-        train_features, test_features, train_targets, test_targets = train_test_split(
+        self.train_features, self.test_features, self.train_targets, self.test_targets = train_test_split(
                                 np.array(self.morgan),
                                 np.array(self.activity),
                                 test_size = 0.10,
                                 random_state=self.itter,
                                 shuffle=True)
-
-        self.train_features = train_features  
-        self.test_features = test_features  
-        self.train_targets = train_targets  
-        self.test_targets = test_targets 
-
-        
-        scores = cross_validate(
+        # Cross validate
+        self.cv_scores = cross_validate(
                     self.regressor, 
                     self.train_features, 
                     self.train_targets,
@@ -81,35 +49,59 @@ class CrossValidator:
                     scoring=('r2', 'explained_variance'),
                     return_train_score=True)
         
+        # Predict cross validation
         self.predictions_cv = cross_val_predict(self.regressor,
                                                 self.train_features,
                                                 self.train_targets, 
                                                 cv=20)
+        
+        self.cv_errors = abs(self.predictions_cv - self.train_targets)
 
-        self._report_cv(scores)
-        self._report_diff()
+        self.cv_auc = self._calculate_auc(auc_thresh=6)
 
+        self._report_score()
+        self._report_errors()
+        self._report_auc()
+        
         self._save_diff()
         self._save_targets()
 
-    def _report_cv(self, scores):        
+    def _report_auc(self):
+        print('The AUC score for loop is: ' + str(self.cv_auc))
+    
+    def _report_score(self):        
         print('The CV variance for loop '+str(self.itter)+' is:', 
-                scores['test_explained_variance'].mean())
+                self.cv_scores['test_explained_variance'].mean())
         print('The CV sigma variance for loop '+str(self.itter)+' is:', 
-                scores['test_explained_variance'].std() * 2)
+                self.cv_scores['test_explained_variance'].std() * 2)
         print('The CV q2 for loop '+str(self.itter)+' is:', 
-                scores['train_r2'].mean())
+                self.cv_scores['train_r2'].mean())
         print('The CV sigma q2 for loop '+str(self.itter)+' is:',
-                scores['train_r2'].std() * 2)
+                self.cv_scores['train_r2'].std() * 2)
 
-    def _report_diff(self):
+    def _report_errors(self):
         
-        errors_cv = abs(self.predictions_cv - self.train_targets)
-
         print ('Calculating CV Mean Absolute Error for loop '+str(self.itter))
-        print('Mean CV-Absolute Error:', round(np.mean(errors_cv), 2), 'units.')
-        print('Standard error:',  round(np.std(errors_cv), 2) )
-        print ("Largest CV prediction error is", np.max(errors_cv))
+        print('Mean CV-Absolute Error:', round(np.mean(self.cv_errors), 2), 'units.')
+        print('Standard error:',  round(np.std(self.cv_errors), 2) )
+        print ("Largest CV prediction error is", np.max(self.cv_errors))
+
+    def _calculate_auc(self, auc_thresh):
+        ''' 
+        Calculate the AUC (Area Under The Curve) of 
+        the ROC (Receiver Operating Characteristics...)
+
+        ROC and AUC are reponses of binary hit based on a threshold.
+        The results need to be converted to binary.
+        '''      
+
+        train_targets_bin = self._binary_convert(self.train_targets, auc_thresh)
+        predictions_bin   = self._binary_convert(self.predictions_cv, auc_thresh)
+
+        self.fpr, self.tpr, self.thresholds = metrics.roc_curve(
+                train_targets_bin,predictions_bin,pos_label=1)
+        
+        return  metrics.auc(self.fpr, self.tpr)
 
     def _save_diff(self):
         
@@ -140,30 +132,8 @@ class CrossValidator:
         result = []
         for val in data:
             if val > thresh:
-                result.appent(1.0)
+                result.append(1.0)
             else:
                 result.append(0.0)
-
-
-    def _record_aoc_curve(self):
-        ''' 
-        Calculate the AUC (Area Under The Curve) of 
-        the ROC (Receiver Operating Characteristics...)
-
-        ROC and AUC are reponses of binary hit based on a threshold.
-        The results need to be converted to binary.
-        '''
         
-        roc_auc_thresh = 6
-        
-
-        train_targets_bin = self._binary_convert(self.train_argets, roc_auc_thresh)
-        predictions_bin   = self._binary_convert(self.predictions_cv, roc_auc_thresh)
-
-        self.fpr, self.tpr, self.thresholds = metrics.roc_curve(
-                train_targets_bin,predictions_bin,pos_label=1)
-        
-
-
-
-    
+        return result
